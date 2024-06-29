bdd600822cc812e258623c76b501e77ae5763fe0
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-06-28 22:21:34 -0400

    she shakes to the left, but ends up on the floor

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -6,30 +6,29 @@ const SideNav: React.FC = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <aside className={`w-full lg:w-64 bg-gray-200 border-r border-gray-300 lg:border-r-0 lg:block lg:sticky lg:top-0 ${darkMode ? 'dark:bg-gray-800' : ''}`}>
+    <aside className={`lg:flex lg:flex-col lg:w-64 lg:bg-gray-200 lg:border-r lg:border-gray-300 ${darkMode ? 'dark:bg-gray-800' : ''}`}>
       <nav className="py-4">
-        <ul>
-          <li className={`px-4 py-2 hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={`px-4 py-2 hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/explorer">Explorer</Link>
-          </li>
-          <li className={`px-4 py-2 hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/portfolio">Portfolio</Link>
-          </li>
-          <li className={`px-4 py-2 hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/defi">Defi</Link>
-          </li>
-        </ul>
-        
         {/* Dark Mode Toggle Button */}
         <button
           onClick={toggleDarkMode}
-          className={`ml-4 mt-4 p-2 border rounded ${darkMode ? 'bg-gray-200 dark:bg-gray-700 dark:text-white' : 'bg-gray-200'}`}
+          className={`mt-4 p-2 ${darkMode ? 'dark:text-white' : 'text-black'}`}
         >
-          Toggle {darkMode ? 'Light' : 'Dark'} Mode
+          {darkMode ? 'Light' : 'Dark'} Mode
         </button>
+        <ul className="flex flex-wrap lg:block lg:space-y-1">
+          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
+            <Link href="/explorer">Explorer</Link>
+          </li>
+          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
+            <Link href="/defi">Defi</Link>
+          </li>
+        </ul>
       </nav>
     </aside>
   );

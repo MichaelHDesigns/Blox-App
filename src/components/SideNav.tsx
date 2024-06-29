f30785eb583c0b3b83caa2311e013cd93da2f215
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2024-06-29 00:47:50 -0400

    her way is coming together, but she's still needing some work

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,35 +1,190 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useDarkMode } from "../contexts/DarkModeContext";
+import {
+  RiHome2Line,
+  RiAccountCircleLine,
+  RiMoneyDollarCircleLine,
+  RiTableAltLine,
+  RiAlarmWarningLine,
+  RiGlobalLine,
+  RiTwitterFill,
+  RiTiktokFill,
+  RiCollapseDiagonalFill,
+  RiLightbulbFlashLine,
+} from "react-icons/ri"; // Import icons from react-icons
 
 const SideNav: React.FC = () => {
   const { darkMode, toggleDarkMode } = useDarkMode();
+  const [collapsed, setCollapsed] = useState(false); // Start with sidebar open
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
 
   return (
-    <aside className={`lg:flex lg:flex-col lg:w-64 lg:bg-gray-200 lg:border-r lg:border-gray-300 ${darkMode ? 'dark:bg-gray-800' : ''}`}>
-      <nav className="py-4">
-        {/* Dark Mode Toggle Button */}
-        <button
-          onClick={toggleDarkMode}
-          className={`mt-4 p-2 ${darkMode ? 'dark:text-white' : 'text-black'}`}
-        >
-          {darkMode ? 'Light' : 'Dark'} Mode
-        </button>
-        <ul className="flex flex-wrap lg:block lg:space-y-1">
-          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/">Home</Link>
+    <aside
+      className={`fixed bottom-0 left-0 max-h-[calc(100vh-5rem)] lg:max-h-full lg:bg-gray-200 lg:border-r lg:border-gray-300 ${
+        darkMode ? "dark:bg-gray-800" : ""
+      } lg:static lg:border-l lg:border-t transform lg:translate-x-0 ${
+        collapsed ? "w-20 lg:w-20" : "w-full lg:w-64"
+      }`}
+      style={{ transition: "width 0.3s ease, transform 0.3s ease" }}
+    >
+      <nav className="py-4 overflow-y-auto lg:overflow-y-hidden">
+        <ul className="flex flex-wrap lg:block lg:space-y-1 mt-4">
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/" legacyBehavior>
+              <a className="flex items-center">
+                <RiHome2Line
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Home</span>}
+              </a>
+            </Link>
           </li>
-          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/explorer">Explorer</Link>
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/portfolio" legacyBehavior>
+              <a className="flex items-center">
+                <RiAccountCircleLine
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Portfolio</span>}
+              </a>
+            </Link>
           </li>
-          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/portfolio">Portfolio</Link>
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/explorer" legacyBehavior>
+              <a className="flex items-center">
+                <RiTableAltLine
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Explorer</span>}
+              </a>
+            </Link>
           </li>
-          <li className={`px-4 py-2 w-full lg:w-auto ${darkMode ? 'text-white' : 'text-black'} hover:bg-gray-300 ${darkMode ? 'dark:hover:bg-gray-700' : ''}`}>
-            <Link href="/defi">Defi</Link>
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/defi" legacyBehavior>
+              <a className="flex items-center">
+                <RiMoneyDollarCircleLine
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Defi</span>}
+              </a>
+            </Link>
+          </li>
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/alerts" legacyBehavior>
+              <a className="flex items-center">
+                <RiAlarmWarningLine
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Alerts</span>}
+              </a>
+            </Link>
+          </li>
+          <li
+            className={`px-4 py-2 w-full lg:w-auto ${
+              darkMode ? "text-white" : "text-black"
+            } hover:bg-gray-300 ${darkMode ? "dark:hover:bg-gray-700" : ""}`}
+          >
+            <Link href="/developers" legacyBehavior>
+              <a className="flex items-center">
+                <RiGlobalLine
+                  className={`mr-2 ${
+                    collapsed ? "icon-collapse" : "text-base"
+                  }`}
+                />
+                {!collapsed && <span>Developers</span>}
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
+
+      {/* Collapse Icon */}
+      <div
+        className={`absolute bottom-4 left-0 ${
+          collapsed ? "lg:left-2" : "lg:left-20"
+        }`}
+      >
+        <button
+          onClick={toggleSidebar}
+          className={`p-2 ${
+            darkMode ? "dark:text-white" : "text-black"
+          } hover:bg-gray-300 rounded-full ${
+            darkMode ? "dark:hover:bg-gray-700" : ""
+          }`}
+        >
+          <RiCollapseDiagonalFill />
+        </button>
+      </div>
+
+      {/* Social Media Icons - Displayed inline */}
+      {!collapsed && (
+        <div className="mt-4 px-4 flex space-x-4 justify-center">
+          <a
+            href="https://twitter.com/BloxSolutions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${
+              darkMode ? "text-white" : "text-black"
+            } hover:text-gray-600 flex items-center`}
+          >
+            <RiTwitterFill className="mr-2" />
+          </a>
+          <a
+            href="https://tiktok.com/@bloxsolutions"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${
+              darkMode ? "text-white" : "text-black"
+            } hover:text-gray-600 flex items-center`}
+          >
+            <RiTiktokFill className="mr-2" />
+          </a>
+        </div>
+      )}
+
+        {/* Dark Mode Toggle Button */}
+        <button
+          onClick={toggleDarkMode}
+          className={`mt-4 p-2 ${
+            darkMode ? "dark:text-white" : "text-black"
+          } text-2xl flex items-center`} // Example of making icons bigger
+        >
+          <RiLightbulbFlashLine />
+        </button>
     </aside>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,15 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+    <main className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome to Blox App!
         </p>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="mb-16 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
         <Link legacyBehavior href="/explorer" passHref>
           <a className={`group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 dark:hover:border-neutral-700 dark:hover:bg-neutral-800/30`}>
             <h2 className="mb-3 text-2xl font-semibold">
